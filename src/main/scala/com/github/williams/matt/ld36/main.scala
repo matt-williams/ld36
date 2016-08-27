6cfb51ca7cc6f8ac01ad12570fa8bd4dbc9e75db
Matt Williams <matwilliams@hotmail.com>
2016-08-27 22:18:26 +0100

    Add Golem

--- src/main/scala/com/github/williams/matt/ld36/main.scala
+++ src/main/scala/com/github/williams/matt/ld36/main.scala
@@ -1,13 +1,31 @@
 package com.github.williams.matt.ld36
 
 import scala.scalajs.js
+import scala.scalajs.js.annotation._
 import scala.scalajs.js.JSApp
+import scala.math.Pi
 import scala.util.Random
 import org.scalajs.dom
 import org.scalajs.dom.raw.{ HTMLElement, HTMLImageElement }
 import org.denigma.threejs._
 import org.denigma.threejs.extensions.SceneContainer
 import org.denigma.threejs.extensions.controls.{ HoverControls, CameraControls }
+
+@js.native
+@JSName("THREE.AnimationMixer")
+class AnimationMixer extends js.Object {
+  def this(root: org.denigma.threejs.Scene = js.native) = this()
+  def clipAction(clip: js.Any, root: js.Any): AnimationMixerAction = js.native
+  def update(delta: Double): AnimationMixer = js.native
+}
+
+@js.native
+@JSName("THREE.AnimationMixer._Action")
+class AnimationMixerAction extends js.Object {
+  def play(): AnimationMixerAction = js.native
+  def stop(): AnimationMixerAction = js.native
+  def reset(): AnimationMixerAction = js.native
+}
 
 object Main extends JSApp {
   def main(): Unit = {
@@ -42,24 +60,26 @@ trait Container3D extends SceneContainer {
   }
 
   val controls: CameraControls = new HoverControls(camera, this.container)
+  val mixer = new AnimationMixer(scene)
 
   container.appendChild(renderer.domElement)
 
   override def onEnterFrame(): Unit = {
     controls.update()
+    mixer.update(0.015)
     renderer.render(scene, camera)
   }
 }
 
 class Scene(val container: HTMLElement, val width: Double, val height: Double) extends Container3D {
-  var textureLoader = new TextureLoader();
+  val textureLoader = new TextureLoader();
   val jsonLoader = new JSONLoader();
   jsonLoader.load("tunnel.json", (geometry: JSonLoaderResultGeometry, materials: js.Array[Material]) => {
     textureLoader.load("tunnel.png", (texture: Texture) => {
       val material = new MeshBasicMaterial(js.Dynamic.literal(
           map = texture
         ).asInstanceOf[MeshBasicMaterialParameters]);
-      for (x <- 0 to 9) {
+      for (x <- -9 to 9) {
         val mesh = new Mesh(geometry, material);
         mesh.position.set(0, 0, x * -2000);
         mesh.scale.set(500, 500, 500);
@@ -69,36 +89,30 @@ class Scene(val container: HTMLElement, val width: Double, val height: Double) e
     ()
   });
 
+  jsonLoader.load("golem.json", (geometry: JSonLoaderResultGeometry, materials: js.Array[Material]) => {
+    textureLoader.load("golem.png", (texture: Texture) => {
+      val material = new MeshBasicMaterial(js.Dynamic.literal(
+          map = texture,
+          skinning = true
+        ).asInstanceOf[MeshBasicMaterialParameters]);
+      val anyGeometry = js.Dynamic.literal(geometry = geometry).geometry;
+      println(anyGeometry.animations.selectDynamic("0").name);
+      println(anyGeometry.animations.selectDynamic("1").name);
+      println(anyGeometry.animations.selectDynamic("2").name);
+      for (x <- -9 to 9) {
+        val mesh = new SkinnedMesh(geometry, material);
+        mesh.position.set(-100, -400, x * -2000);
+        mesh.scale.set(500, 500, 500);
+        mesh.rotateY(Pi / 2);
+        scene.add(mesh);
 
-/*
-  val geometry = new BoxGeometry(350, 300, 250)
-
-  val colors = List("green", "red", "blue", "orange", "purple", "teal")
-  val colorMap = Map(colors.head -> 0xA1CF64, colors(1) -> 0xD95C5C, colors(2) -> 0x6ECFF5,
-    colors(3) -> 0xF05940, colors(4) -> 0x564F8A, colors(5) -> 0x00B5AD)
-
-  def materialParams(name: String): MeshLambertMaterialParameters = js.Dynamic.literal(
-    color = new Color(colorMap(name))
-    ).asInstanceOf[MeshLambertMaterialParameters]
-
-  def randColorName: String = colors(Random.nextInt(colors.size))
-
-  var meshes = addMesh(new Vector3(0, 0, 0)) :: addMesh(new Vector3(400, 0, 200)) :: addMesh(new Vector3(-400, 0, 200)) :: Nil
-*/
+        mixer.clipAction(anyGeometry.animations.selectDynamic("0"), mesh).play();
+      }
+    });
+    ()
+  });
 
   val light = new DirectionalLight(0xffffff, 2)
   light.position.set(1, 1, 1).normalize()
   scene.add(light)
-
-/*
-  def addMesh(pos: Vector3 = new Vector3()): Mesh = {
-    val material = new MeshLambertMaterial(this.materialParams(randColorName))
-    val mesh: Mesh = new Mesh(geometry, material)
-    mesh.name = pos.toString
-    mesh.position.set(pos.x, pos.y, pos.z)
-    mesh
-  }
-
-  meshes.foreach(scene.add)
-*/
 }
