a5ee6db3bcb1c31bcdb0905473ccd50a9f770ab6
Matt Williams <matwilliams@hotmail.com>
2016-08-28 10:15:03 +0100

    Golems walk around on API command

--- src/main/scala/com/github/williams/matt/ld36/main.scala
+++ src/main/scala/com/github/williams/matt/ld36/main.scala
@@ -3,7 +3,7 @@ package com.github.williams.matt.ld36
 import scala.scalajs.js
 import scala.scalajs.js.annotation._
 import scala.scalajs.js.JSApp
-import scala.math.Pi
+import scala.math.{ Pi, min, max }
 import scala.util.Random
 import org.scalajs.dom
 import org.scalajs.dom.raw.{ HTMLElement, HTMLImageElement }
@@ -26,6 +26,7 @@ class AnimationMixerAction extends js.Object {
   def stop(): AnimationMixerAction = js.native
   def reset(): AnimationMixerAction = js.native
   def startAt(time: Double): AnimationMixerAction = js.native
+  def isRunning(): Boolean = js.native
   def setLoop(mode: Int, repetitions: Int): AnimationMixerAction = js.native
 }
 
@@ -40,14 +41,19 @@ object MyTHREE extends js.Object {
 object Main extends JSApp {
   def main(): Unit = {
     val el: HTMLElement = dom.document.getElementById("container").asInstanceOf[HTMLElement]
-    val scene = new Scene(el, 1280, 500)
+/*
+    def resize() = {
+      el.width = dom.window.innerWidth;
+      el.height = dom.window.innerHeight;
+    }
+    val scene = new Scene(el, el.width, el.height)
+*/
+    val scene = new Scene(el, dom.window.innerWidth, dom.window.innerHeight)
     scene.render()
   }
 }
 
 trait Container3D extends SceneContainer {
-  container.style.width = width.toString
-  container.style.height = height.toString
   container.style.position = "relative"
 
   override type RendererType = WebGLRenderer
@@ -79,6 +85,70 @@ trait Container3D extends SceneContainer {
   }
 }
 
+class Golem(geometry: Geometry, material: MeshBasicMaterial, mixer: AnimationMixer) {
+  private val mesh = new SkinnedMesh(geometry, material)
+  mesh.scale.set(500, 500, 500);
+  mesh.position.set(-100, -400, 0);
+
+  val object3d = new Object3D()
+  object3d.add(mesh)
+
+  val position = object3d.position;
+  var _orientation: Double = 0;
+  val up: Vector3 = new Vector3(0, 1, 0)
+  val forward: Vector3 = new Vector3(1, 0, 0)
+  def orientation = _orientation;
+  def orientation_= (value: Double):Unit = {
+    object3d.rotateY(value - _orientation)
+    forward.applyAxisAngle(up, value - _orientation);
+    _orientation = value;
+  }
+
+  private val walkBegin = mixer.clipAction("walk.begin", mesh)
+  private val walkCycle = mixer.clipAction("walk.cycle", mesh)
+  private val walkEnd = mixer.clipAction("walk.end", mesh)
+
+  private val target = new Vector3()
+  def walkTo(x: Double, z: Double): Unit = {
+    target.set(x, 0, z)
+    if ((position.distanceTo(target) > 1) &&
+        (!walkBegin.isRunning()) &&
+        (!walkCycle.isRunning())) {
+      walkEnd.stop();
+      walkBegin.setLoop(MyTHREE.LoopOnce, 1).play();
+      walkCycle.startAt(0.833333).play();
+    }
+  }
+
+  def update(delta: Double): Unit = {
+    if (walkBegin.isRunning() || walkCycle.isRunning() || walkEnd.isRunning()) {
+      val bearingVector = target.clone().sub(position)
+      val bearing = forward.angleTo(bearingVector)
+      val bearingDelta = min(Pi / 32, bearing)
+      val trialForward = forward.clone().applyAxisAngle(up, bearingDelta)
+      if (trialForward.angleTo(bearingVector) > bearing) {
+        orientation -= bearingDelta
+      } else {
+        orientation += bearingDelta
+      }
+      if ((position.distanceTo(target) < 600 * 1.66666) &&
+          ((walkBegin.isRunning()) ||
+           (walkCycle.isRunning()))) {
+        walkBegin.stop();
+        walkCycle.stop();
+        walkEnd.play();
+      }
+      // object3d automatically considers forward vector when translating
+      if (position.distanceTo(target) < 600 * delta) {
+        walkEnd.stop();
+        object3d.translateX(position.distanceTo(target));
+      } else {
+        object3d.translateX(600 * delta);
+      }
+    }
+  }
+}
+
 class Scene(val container: HTMLElement, val width: Double, val height: Double) extends Container3D {
   val mixer = new AnimationMixer(scene)
 
@@ -99,7 +169,7 @@ class Scene(val container: HTMLElement, val width: Double, val height: Double) e
     ()
   });
 
-  var golem: Option[SkinnedMesh] = None;
+  var golems: List[Golem] = List()
 
   jsonLoader.load("golem.json", (geometry: JSonLoaderResultGeometry, materials: js.Array[Material]) => {
     textureLoader.load("golem.png", (texture: Texture) => {
@@ -111,27 +181,19 @@ class Scene(val container: HTMLElement, val width: Double, val height: Double) e
       for (i <- 0 to 2) {
         mixer.clipAction(anyGeometry.animations.selectDynamic(i.toString), ());
       }
-      val mesh = new SkinnedMesh(geometry, material);
-      mesh.position.set(0, -400, 18000 - 100);
-      mesh.scale.set(500, 500, 500);
-      mesh.rotateY(Pi / 2);
-      scene.add(mesh);
-      mixer.clipAction("walk.begin", mesh).setLoop(MyTHREE.LoopOnce, 1).play();
-      mixer.clipAction("walk.cycle", mesh).startAt(0.833333).play();
-      golem = Some(mesh);
-/*
-      for (z <- -9 to 9) {
-        val mesh = new SkinnedMesh(geometry, material);
-        mesh.position.set(-100, -400, z * -2000);
-        mesh.scale.set(500, 500, 500);
-        mesh.rotateY(Pi / 2);
-        scene.add(mesh);
+      val golem = new Golem(geometry, material, mixer);
+      scene.add(golem.object3d);
+      golem.position.set(0, 0, -18000);
+      //golem.orientation = -Pi / 2;
+      golem.walkTo(0, -2000)
+      golems = golem :: golems;
 
-        mixer.clipAction("walk.begin", mesh).setLoop(MyTHREE.LoopOnce, 1).play();
-        mixer.clipAction("walk.cycle", mesh).startAt(0.833333).play();
-        //mixer.clipAction("walk.end", mesh).play();
-      }
-*/
+      val golem2 = new Golem(geometry, material, mixer);
+      scene.add(golem2.object3d);
+      golem2.position.set(0, 0, 18000);
+      golem2.orientation = Pi / 2;
+      golem2.walkTo(0, 2000)
+      golems = golem2 :: golems;
     });
     ()
   });
@@ -141,10 +203,9 @@ class Scene(val container: HTMLElement, val width: Double, val height: Double) e
   scene.add(light)
 
   override def onEnterFrame(): Unit = {
-    mixer.update(0.015)
-    golem match {
-      case Some(mesh) => mesh.translateX(600 * 0.015)
-      case None => ()
+    mixer.update(0.03)
+    for (golem <- golems) {
+      golem.update(0.03)
     }
     
     super.onEnterFrame()
